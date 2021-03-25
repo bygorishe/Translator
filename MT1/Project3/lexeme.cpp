a499fee30851d9ab8f0cc7f9ae8044f50a6ed4b6
bygorishe <81306295+bygorishe@users.noreply.github.com>
2021-03-25 16:46:29 +0700

    Add files via upload

---
+++ MT1/Project3/lexeme.cpp
@@ -0,0 +1,12 @@
+#include "lexeme.h"
+// Конструктор по умолчанию
+lexeme::lexeme() {}
+// Конструктор с заданием имени идентификатора или значения константы
+lexeme::lexeme(string new_name)
+{
+	name = new_name;
+	type = 0;
+	is_init = false;
+}
+// Деструктор
+lexeme::~lexeme() {}
